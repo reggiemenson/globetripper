008f5f5451c2c4616ca4ecd40c97fe33b950b0d9
reggiemenson <44327202+reggiemenson@users.noreply.github.com>
2022-03-08 15:11:39 +0000

    format clearing and import clean

--- jwt_auth/views.py
+++ jwt_auth/views.py
@@ -1,31 +1,21 @@
-from jwt_auth.serializers import UserSerializer, PopulatedUserSerializer
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.exceptions import PermissionDenied, ValidationError
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
-from rest_framework.status import HTTP_201_CREATED, HTTP_422_UNPROCESSABLE_ENTITY, HTTP_204_NO_CONTENT, HTTP_401_UNAUTHORIZED
-
-from django.conf import settings
 import jwt
 
-from django.db.models import F
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.status import HTTP_422_UNPROCESSABLE_ENTITY, HTTP_204_NO_CONTENT
 
 from .badge_logic import get_platform_badges, get_user_badges, get_user_score
-from django.contrib.auth import get_user_model
+from .serializers import ValidateSerializer, UserSerializer, PopulatedUserSerializer
+
 User = get_user_model()
 
-from .serializers import ValidateSerializer, UserSerializer, PopulatedUserSerializer 
 
 class RegisterView(APIView):
-
-    # def get_user(self, request):
-    #     serializer = ValidateSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response({'message': 'Registration successful'})
-
-    #     return Response(serializer.errors, status=422)
 
     def post(self, request):
         serializer = ValidateSerializer(data=request.data)
@@ -72,7 +62,7 @@ class ProfileView(APIView):
     def put(self, request):
         initial_user = User.objects.get(pk=request.user.id)
         updated_user = UserSerializer(initial_user, data=request.data)
-        if (updated_user.is_valid()):
+        if updated_user.is_valid():
             initial_user = updated_user
             initial_user.save()
             return Response(initial_user.data)
@@ -84,7 +74,8 @@ class ProfileView(APIView):
         return Response(status=HTTP_204_NO_CONTENT)
 
         # The following view is for editing the user preference points (badges, towns, etc) that will affect ranking.
-        
+
+
 class EditDetailView(APIView):
 
     permission_classes = (IsAuthenticated, )
@@ -92,7 +83,7 @@ class EditDetailView(APIView):
     def put(self, request):
         initial_user = User.objects.get(pk=request.user.id)
         updated_user = UserSerializer(initial_user, data=request.data)
-        if (updated_user.is_valid()):
+        if updated_user.is_valid():
             initial_user = updated_user
             initial_user.save()
             new_user = User.objects.get(pk=request.user.id)
@@ -108,13 +99,13 @@ class EditDetailView(APIView):
             badge_user.data['badges'].extend(badges)
 
             updated_badge_user = UserSerializer(test_user, data=badge_user.data)
-            if (updated_badge_user.is_valid()):
+            if updated_badge_user.is_valid():
                 test_user = updated_badge_user
                 test_user.save()
 
                 all_users = User.objects.all()
-                serialized_userList = PopulatedUserSerializer(all_users, many=True)
-                get_platform_badges(serialized_userList)
+                serialized_users = PopulatedUserSerializer(all_users, many=True)
+                get_platform_badges(serialized_users)
 
                 return Response(test_user.data)
             return Response(updated_badge_user.errors, status=HTTP_422_UNPROCESSABLE_ENTITY)
@@ -129,16 +120,6 @@ class UserView(APIView):
         user = User.objects.get(pk=pk)
         serialized_user = PopulatedUserSerializer(user)
         return Response(serialized_user.data)
-
-
-    # def post(self, request, pk): 
-    #         request.data['owner'] = request.user.id 
-    #         request.data['post'] = pk # attach the post id from the url to comment
-    #         town = TownSerializer(data=request.data) # serialize the comment
-    #         if town.is_valid(): # if the comment is valid
-    #             town.save() # save the comment
-    #         return Response(town.errors, status=HTTP_422_UNPROCESSABLE_ENTITY) # send back any errors from the comment if it wasnt valid
-
     
 
 class UserListView(APIView):
@@ -146,10 +127,6 @@ class UserListView(APIView):
     permission_classes = (IsAuthenticatedOrReadOnly, )
 
     def get(self, request):
-        print('reached?')
         users = User.objects.all()
-        serialized_userList = PopulatedUserSerializer(users, many=True)
-        return Response(serialized_userList.data)
-
-
-         
+        serialized_users = PopulatedUserSerializer(users, many=True)
+        return Response(serialized_users.data)
